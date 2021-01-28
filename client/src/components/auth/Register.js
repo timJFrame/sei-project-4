105d0da940031739482eb41c4bf8330c81122a09
timframe <t.f.retouching@gmail.com>
2021-01-28 18:53:07 +0000

    started wiring up auth forms

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,22 +1,68 @@
 import React from 'react'
 
 function Register(){
+
+  const [formdata, setFormdata] = React.useState({
+    username: '',
+    email: '',
+    password: '',
+    password_confirmation: '',
+    bio: '',
+    profile_image: ''
+  })
+
+  const handleChange = (e) => {
+    setFormdata({ ...formdata, [e.target.name]: e.target.value })
+  }
+
+  console.log(formdata)
+
   return (
     <section className="form-container">
       <form>
         <fieldset>
           <label>Username</label>
-          <input type="text" placeholder="Username"/>
+          <input type="text" 
+            name="username"
+            value={formdata.username}
+            onChange={handleChange}
+            placeholder="Username"/>
+
+
           <label>Email</label>
-          <input type="text" placeholder="Email"/>
+          <input type="text" 
+            name="email"
+            value={formdata.email}
+            onChange={handleChange}
+            placeholder="Email"/>
+
+
           <label>Password</label>
-          <input type="password" placeholder="Password"/>
+          <input type="password" 
+            name="password"
+            value={formdata.password}
+            onChange={handleChange}
+            placeholder="Password"/>
+
+
           <label>Password Confirmation</label>
-          <input type="password" placeholder="Password Confirmation"/>
+          <input type="password"
+            name="password_confirmation"
+            value={formdata.password_confirmation}
+            onChange={handleChange}
+            placeholder="Password Confirmation"/>
+
+
           <label>Bio</label>
-          <textarea placeholder="Tell why you love gardening"/>
+          <textarea
+            name="bio"
+            value={formdata.bio}
+            onChange={handleChange}
+            placeholder="Tell why you love gardening"/>
           <label>Profile Photo</label>
           <input type="file"/>
+
+
         </fieldset>
         <input className="button-outline form-sumbit full-length-button" type="submit" value="Register"/>
       </form>
